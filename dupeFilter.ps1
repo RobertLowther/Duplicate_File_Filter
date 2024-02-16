98e84dc869e90ffb077702cbc79872c19f4b78d0
Robert Lowther <drail159@gmail.com>
2024-02-15 22:01:54 -0700

    Account for relative and absolute path in params, insert comments

--- dupeFilter.ps1
+++ dupeFilter.ps1
@@ -1,19 +1,65 @@
 param (
     [string]$path,
+    [string]$dest,
     [switch]$check = $false
 )
 
-$target_directory = "$(Get-Location)\$($path)"
-
-if (-not (Test-Path -Path $target_directory)) {
-    Write-Output "Location '$($target_directory)' does not exist"
+############### Validate Target ################
+# Validate target directory structure
+if (-not (Test-Path -Path $path -IsValid)) {
+    Write-Output "$($target_directory) is not a valid path."
     exit
+}
+
+$target_directory = [IO.Path]::GetFullPath($path)
+
+# Validate target directory exists
+if (-not (Test-Path -Path $target_directory)) {
+    Write-Output "$($target_directory) does not exist."
+    exit
+}
+
+################ Validate destination ################
+# Validate dest directory structure
+if (-not (Test-Path -Path $dest -IsValid)) {
+    Write-Output "$($dest) is not a valid path"
+}
+
+# Create full destination path
+if ([IO.Path]::IsPathRooted($dest)) {
+    $dupe_directory = [IO.Path]::GetFullPath($dest)
+} else {
+    $joined = Join-Path $target_directory $dest
+    $dupe_directory = [IO.Path]::GetFullPath($joined)
+}
+
+# Modify destination path such that it is a dir which doesn't exist
+if (Test-Path -Path "$($dupe_directory)"){
+    $i = 0
+
+    while ($true) {
+        $i++
+
+        if (-not (Test-Path -Path "$($dupe_directory)$($i)")) {
+            $dupe_directory += $i
+            break
+        }
+
+        if ($i -ge 255) 
+        {
+            Write-Output "$($dupe_directory) is not a valid output location, please select another name for the output folder"
+        }
+    }
 }
 
 Write-Output "`nChecking for possible duplicated files in $($target_directory)"
 
 $file_list = Get-ChildItem -Path $target_directory
 
+# construct a dictionary of file whos names suggest that they may be duplicates
+# Source $file_list
+# Keys - Suspected original
+# Values - Array of suspected duplicates
 $filtered_files = @{}
 
 foreach ($file in $file_list) {
@@ -46,6 +92,10 @@ foreach ($file in $file_list) {
     }
 }
 
+# Create a dictionary of files whos length suggests they might be duplicates
+# Source - $filterd_files
+# Keys - Suspected original
+# Values - Suspected duplicates
 $dupe_table = @{}
 
 foreach ($original in $filtered_files.keys) {
@@ -66,25 +116,28 @@ foreach ($original in $filtered_files.keys) {
     }
 }
 
-if (($dupe_table.Keys.Length -gt 0) -and -not (Test-Path -Path "$($target_directory)\dupe")) {
-    New-Item -Path "$($target_directory)\dupe" -ItemType Directory
+if ($dupe_table.Count -eq 0) {
+    Write-Output "No potential duplicates found"
+    exit
 }
 
-if ($test) {
+if ($check) {
     Write-Output "The following itmes have been identified as possible duplicates:`n"
     foreach ($original in $dupe_table.keys) {
         foreach ($dupe in $dupe_table[$original]) {
             Write-Output "`t$($dupe)"
         }
     }
-    Write-Output "`nThese items would be moved to $($target_directory)\dupe\`n"
+    Write-Output "`nThese items would be moved to $dupe_directory`n"
     exit
 }
+
+New-Item -Path $dupe_directory -ItemType Directory
 
 foreach ($original in $dupe_table.keys) {
     foreach ($dupe in $dupe_table[$original]) {
         Write-Output "src: $($target_directory)\$($dupe)"
-        $destination =  "$($target_directory)\dupe\$($dupe)"
+        $destination =  "$($dupe_directory)\$($dupe)"
         Write-Output "dst: $($destination)"
         Write-Output ""
         if ($destination.Length -gt 256) {
